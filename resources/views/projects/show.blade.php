bd3494b549f6ab3e616b1b87ac63905d7e09f095
mindazub2025 <mindaugas.azubalis@edislab.lt>
2025-04-17 14:36:29 +0300

    project company plant device table columns are equall always

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -74,45 +74,49 @@
                     <div class="card mb-5">
                         <div class="card-body">
                             <h4 class="mb-4">Project Plant and Device List</h4>
-                            <table class="table table-striped table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>Project</th>
-                                        <th>Company</th>
-                                        <th>Plant</th>
-                                        <th>Device</th>
-                                    </tr>
-                                </thead>
-                                <tbody id="device-table-body">
-                                    @php $rowCount = 0; @endphp
-                                    @foreach ($project->companies as $company)
-                                        @foreach ($company->plants as $plant)
-                                            @php $deviceCount = $plant->devices->count(); @endphp
+                            <div class="table-responsive">
+                                <table
+                                    class="table table-striped table-bordered table-sm table-hover align-middle text-center">
+                                    <thead class="table-light">
+                                        <tr>
+                                            <th style="width: 25%;">Project</th>
+                                            <th style="width: 25%;">Company</th>
+                                            <th style="width: 25%;">Plant</th>
+                                            <th style="width: 25%;">Device</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="device-table-body">
+                                        @php $rowCount = 0; @endphp
+                                        @foreach ($project->companies as $company)
+                                            @foreach ($company->plants as $plant)
+                                                @php $deviceCount = $plant->devices->count(); @endphp
 
-                                            @if ($deviceCount === 0)
-                                                @php $rowCount++; @endphp
-                                                <tr class="device-row {{ $rowCount > 5 ? 'd-none more-row' : '' }}">
-                                                    <td>{{ $project->name }}</td>
-                                                    <td>{{ $company->name }}</td>
-                                                    <td>{{ $plant->name }}</td>
-                                                    <td class="text-muted">No devices</td>
-                                                </tr>
-                                            @else
-                                                @foreach ($plant->devices as $device)
+                                                @if ($deviceCount === 0)
                                                     @php $rowCount++; @endphp
                                                     <tr
                                                         class="device-row {{ $rowCount > 5 ? 'd-none more-row' : '' }}">
                                                         <td>{{ $project->name }}</td>
                                                         <td>{{ $company->name }}</td>
                                                         <td>{{ $plant->name }}</td>
-                                                        <td>{{ $device->name }}</td>
+                                                        <td class="text-muted">No devices</td>
                                                     </tr>
-                                                @endforeach
-                                            @endif
+                                                @else
+                                                    @foreach ($plant->devices as $device)
+                                                        @php $rowCount++; @endphp
+                                                        <tr
+                                                            class="device-row {{ $rowCount > 5 ? 'd-none more-row' : '' }}">
+                                                            <td>{{ $project->name }}</td>
+                                                            <td>{{ $company->name }}</td>
+                                                            <td>{{ $plant->name }}</td>
+                                                            <td>{{ $device->name }}</td>
+                                                        </tr>
+                                                    @endforeach
+                                                @endif
+                                            @endforeach
                                         @endforeach
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                    </tbody>
+                                </table>
+                            </div>
 
                             @if ($rowCount > 5)
                                 <div class="text-center mt-3">
@@ -126,6 +130,7 @@
                             @endif
                         </div>
                     </div>
+
 
 
 
