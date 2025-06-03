ade5d122a3b53c363b8ce390400b801d6c32ba31
mindazub2025 <mindaugas.azubalis@edislab.lt>
2025-06-03 17:14:37 +0300

    edit DashboardController and removed Company - old model, not using it anymore

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,45 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\Plant;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-use App\Models\Project;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
     use AuthorizesRequests;
-
-    // public function index()
-    // {
-    //     $projects = Project::withCount([
-    //         'companies',
-    //         'companies as plants_count' => fn($q) =>
-    //             $q->join('plants', 'companies.id', '=', 'plants.company_id'),
-    //         'companies as devices_count' => fn($q) =>
-    //             $q->join('plants', 'companies.id', '=', 'plants.company_id')
-    //             ->join('devices', 'plants.id', '=', 'devices.plant_id'),
-    //     ])->latest()->get();
-
-    //     return view('dashboard.index', compact('projects'));
-    // }
-
-
-    public function index()
+    /**
+     * Display the dashboard with a list of plants.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(): View
     {
         $plants = Plant::with('mainFeeds.devices')->paginate();
         return view('plants.index', compact('plants'));
     }
-
-    public function show($id)
-    {
-        $project = Project::with([
-            'companies.plants.devices',
-            'companies.plants',
-        ])->findOrFail($id);
-
-        return view('dashboard.show', compact('project'));
-
-
-}
 
 }
