f60a530e1b0c3c94d33d70c73c015893fe5c41bd
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2025-06-12 14:32:36 +0300

    plus text changed to blue-600

--- resources/views/plants/partials/devices-list.blade.php
+++ resources/views/plants/partials/devices-list.blade.php
@@ -53,14 +53,14 @@
                                                     <td class="px-2 py-2 text-center">
                                                         @if($isParent)
                                                             <button type="button" class="focus:outline-none flex items-center justify-center" aria-expanded="false" aria-label="Toggle children">
-                                                                <svg id="icon-{{ $parentRowId }}" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus w-5 h-5 text-indigo-600 dark:text-indigo-400">
+                                                                <svg id="icon-{{ $parentRowId }}" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus w-5 h-5 text-blue-600 dark:text-blue-400">
                                                                     <circle cx="12" cy="12" r="10"></circle>
                                                                     <path d="M8 12h8"></path>
                                                                     <path d="M12 8v8"></path>
                                                                 </svg>
                                                             </button>
                                                         @else
-                                                            <div class="flex items-center justify-center">
+                                                            <button class="flex items-center justify-center">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" 
                                                                      width="24" height="24" 
                                                                      viewBox="0 0 24 24" 
@@ -72,7 +72,7 @@
                                                                      class="lucide w-5 h-5 text-gray-400 dark:text-gray-500">
                                                                     <circle cx="12" cy="12" r="10"></circle>
                                                                 </svg>
-                                                            </div>
+                                                            </button>
                                                         @endif
                                                     </td>
                                                     <td class="px-2 py-2">
